29502cc2af8fae72047fa5984688bb79266ee12e
ePerez <30679311+Perez1996@users.noreply.github.com>
2021-05-11 11:59:08 -0300

    Create StartJenkins.sh
    Run to start Jenkins Server

---
+++ StartJenkins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker run --rm -u root -p 6666:8080 -v jenkins-data:/var/jenkins_home -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME":/home --name Perez-Carrizo_Jenkins jenkins/jenkins:lts
+
+
